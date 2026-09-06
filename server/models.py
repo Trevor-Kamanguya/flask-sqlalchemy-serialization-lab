8515990789af45e666824332a49caa6639491ef0
Trevor <mtrevorkk@gmail.com>
2026-09-06 22:39:33 +0300

    done

--- server/models.py
+++ server/models.py
@@ -17,6 +17,9 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
@@ -28,5 +31,43 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
+
+
+    class CustomerSchema(Schema):
+    id = fields.Int()
+    name = fields.String()
+    reviews = fields.List(fields.Nested(lambda: ReviewSchema(exclude=('customer',))))
+
+
+class ItemSchema(Schema):
+    id = fields.Int()
+    name = fields.String()
+    price = fields.Float()
+    reviews = fields.List(fields.Nested(lambda: ReviewSchema(exclude=('item',))))
+
+
+class ReviewSchema(Schema):
+    id = fields.Int()
+    comment = fields.String()
+    customer = fields.Nested(lambda: CustomerSchema(exclude=('reviews',)))
+    item = fields.Nested(lambda: ItemSchema(exclude=('reviews',)))
